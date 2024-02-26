1f484e658fb8cac9bede1379935b15ab1c9b8003
andubadea <37139260+andubadea@users.noreply.github.com>
2024-02-26 11:36:54 +0100

    Relative + grayscale env

--- bluesky_gym/__init__.py
+++ bluesky_gym/__init__.py
@@ -8,3 +8,9 @@ def register_envs():
         entry_point="bluesky_gym.envs.conflict_art:ConflictArtEnv",
         max_episode_steps=300,
     )
+    
+    register(
+        id="ConflictArtRel-v0",
+        entry_point="bluesky_gym.envs.conflict_art:ConflictArtRelEnv",
+        max_episode_steps=300,
+    )
