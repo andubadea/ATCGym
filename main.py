e89e7eca20b09ba422a3d1f7153132fec262cc3e
andubadea <37139260+andubadea@users.noreply.github.com>
2024-02-26 11:38:07 +0100

    a2c and rel env in main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ EVAL_EPISODES = 10
 
 if __name__ == "__main__":
     # Create the environment
-    env = gym.make('ConflictArt-v0', render_mode=None)
+    env = gym.make('ConflictArtRel-v0', render_mode=None)
     
     # Can check the environment here
     # check_env(env)
@@ -23,23 +23,23 @@ if __name__ == "__main__":
 
     # Train the model
     if TRAIN:
-        model.learn(total_timesteps=int(12e4))
-        model.save("models/ConflictArt-v0_ppo/model")
+        model.learn(total_timesteps=int(3e6))
+        model.save("models/ConflictArt-v0_a2c/model")
         del model
     
     env.close()
     
     # Test the trained model
-    model = PPO.load("models/ConflictArt-v0_ppo/model", env=env)
-    env = gym.make('ConflictArt-v0', render_mode='human')
+    # model = PPO.load("models/ConflictArt-v0_ppo/model", env=env)
+    # env = gym.make('ConflictArt-v0', render_mode='human')
 
-    for i in range(EVAL_EPISODES):
-        done = truncated = False
-        obs, info = env.reset()
-        while not (done or truncated):
-            # Predict
-            action, _states = model.predict(obs, deterministic=True)
-            # Get reward
-            obs, reward, done, truncated, info = env.step(action[()])
+    # for i in range(EVAL_EPISODES):
+    #     done = truncated = False
+    #     obs, info = env.reset()
+    #     while not (done or truncated):
+    #         # Predict
+    #         action, _states = model.predict(obs, deterministic=True)
+    #         # Get reward
+    #         obs, reward, done, truncated, info = env.step(action[()])
 
     env.close()
