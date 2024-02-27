7e853b3c22ede428fae1c5f98ccfe7b4e3172e64
andubadea <37139260+andubadea@users.noreply.github.com>
2024-02-27 11:26:50 +0100

    better logplotter

--- log_plotter.py
+++ log_plotter.py
@@ -1,27 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open('out.log', 'r') as f:
-    lines = f.readlines()
-    
-rew_mean = []
-timestep_no = []
-    
-for line in lines:
-    if 'ep_rew_mean' in line:
-        split = line.split('|')
-        rew = float(split[2])
-        rew_mean.append(rew)
-    if 'total_timesteps' in line:
-        split = line.split('|')
-        step = int(split[2])
-        timestep_no.append(step)
+def plot_out_log(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
         
-fig, ax = plt.subplots()
-ax.plot(timestep_no, rew_mean)
-ax.hlines(1, 0, max(timestep_no), color = 'red')
-ax.set_xlabel('Number of timesteps')
-ax.set_ylabel('Sum of rewards')
-plt.show()
+    rew_mean = []
+    timestep_no = []
+        
+    for line in lines:
+        if 'ep_rew_mean' in line:
+            split = line.split('|')
+            rew = float(split[2])
+            rew_mean.append(rew)
+        if 'total_timesteps' in line:
+            split = line.split('|')
+            step = int(split[2])
+            timestep_no.append(step)
+            
+    fig, ax = plt.subplots()
+    ax.set_title(filename)
+    ax.plot(timestep_no, rew_mean)
+    ax.hlines(1, 0, max(timestep_no), color = 'red')
+    ax.set_xlabel('Number of timesteps')
+    ax.set_ylabel('Sum of rewards')
+    plt.show()
+    
+plot_out_log('output/out_rgb.log')
+plot_out_log('output/out_rel.log')
+plot_out_log('output/out_gry.log')
 
-#scp sim6:~/Desktop/Andrei/BlueskyGym/out.log out.log
+'''
+scp sim6:~/Desktop/Andrei/BlueskyGym/\*.log  ./output/
+'''
