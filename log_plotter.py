884684c625ca02122f45089209a544b0776ab5da
andubadea <37139260+andubadea@users.noreply.github.com>
2024-07-11 10:38:24 +0200

    Moving average in log plotter.

--- log_plotter.py
+++ log_plotter.py
@@ -1,5 +1,8 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
+
+moving_average_window = 20
 
 def plot_out_log(filename):
     with open(filename, 'r') as f:
@@ -17,19 +20,33 @@ def plot_out_log(filename):
             split = line.split('|')
             step = int(split[2])
             timestep_no.append(step)
+    
+    # Moving average
+    mv_avg = []
+    print(len(rew_mean))
+    for i in range(moving_average_window, len(rew_mean)):
+        mv_avg.append(np.average(rew_mean[i-moving_average_window:i]))
             
     fig, ax = plt.subplots()
-    ax.set_title(filename)
+    ax.set_title(f'{filename} mean reward')
     ax.plot(timestep_no, rew_mean)
     ax.hlines(1, 0, max(timestep_no), color = 'red')
     ax.set_xlabel('Number of timesteps')
+    ax.set_ylabel('Sum of rewards')
+    plt.show()
+    
+    fig, ax = plt.subplots()
+    ax.set_title(f'{filename} moving average')
+    ax.plot(range(len(mv_avg)), mv_avg)
+    ax.hlines(1, 0, len(mv_avg), color = 'red')
+    ax.set_xlabel('Timestep')
     ax.set_ylabel('Sum of rewards')
     plt.show()
 
 logfiles = os.listdir('output')
 
 for filename in logfiles:
-    if '.log' in filename:
+    if 'out.log' in filename:
         plot_out_log(f'output/{filename}')
 
 #scp sim6:~/Desktop/Andrei/BlueskyGym/\*.log  ./output/
